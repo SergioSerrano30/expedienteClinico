4450c0476ae916502ba4f9e983546d7bddb4b965
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-10-27 23:21:18 -0600

    De aqui pa delante

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,6 +1,7 @@
 
 export class Usuario {
-  _id?: number;
+  _id?: string;
+  idUsuario_PK: number;
   usuario: string;
   password: string;
   activo: string;
@@ -20,6 +21,7 @@ export class Usuario {
     persona_domicilio:
       [{
         _id?: string
+        idDomicilio_PK: number
         calle: string,
         numero_EXT: string,
         numero_INT: string,
@@ -30,21 +32,20 @@ export class Usuario {
         pais: string,
         estado: string,
         municipio: string
-        
       }]
   }]
 
-  constructor(usuario: string, password: string, activo: string,
+  constructor(idUsuario_PK:number,usuario: string, password: string, activo: string,
     usuario_rol:[],idRol_PK: number, desRol: string,
     usuario_persona:[],idPersona_PK: number, nombre: string, apPaterno: string,apMaterno: string, fechaNac: string, sexo: string,
-    persona_domicilio:[],calle: string, numero_EXT: string, numero_INT: string, colonia: string,entrecalle1: string,entrecalle2: string,referencia: string,pais: string,estado: string,municipio: string) {
+    persona_domicilio:[],idDomicilio_PK: number,calle: string, numero_EXT: string, numero_INT: string, colonia: string,entrecalle1: string,entrecalle2: string,referencia: string,pais: string,estado: string,municipio: string) {
+    this.idUsuario_PK = idUsuario_PK
     this.usuario = usuario;
     this.password = password;
     this.activo = activo;
     this.usuario_rol = [{idRol_PK,desRol}];
-    
     this.usuario_persona =[{idPersona_PK,nombre,apPaterno,apMaterno,fechaNac,sexo,
-      persona_domicilio:[{calle,numero_EXT,numero_INT,colonia,entrecalle1,entrecalle2,referencia,pais,estado,municipio}]
+      persona_domicilio:[{idDomicilio_PK,calle,numero_EXT,numero_INT,colonia,entrecalle1,entrecalle2,referencia,pais,estado,municipio}]
   }];
   }
   
