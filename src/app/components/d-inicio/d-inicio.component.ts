4bdc3cb9bac30308a05b31caa449487309ab3d74
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-10-27 19:19:02 -0600

    Visualizar subdocumentos

--- src/app/components/d-inicio/d-inicio.component.ts
+++ src/app/components/d-inicio/d-inicio.component.ts
@@ -8,7 +8,6 @@ import { findIndex, Observable } from 'rxjs';
 //import { AuthService } from '@auth0/auth0-angular';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import {Usuario} from 'src/app/models/usuario'
-import { Usuario_Rol } from 'src/app/models/usuario_rol';
 
 @Component({
   selector: 'app-d-inicio',
@@ -17,7 +16,6 @@ import { Usuario_Rol } from 'src/app/models/usuario_rol';
 })
 export class DInicioComponent implements OnInit {
   listUsuarios: Usuario[] = [];
-  listUsuarios_Rol: Usuario_Rol[] = [];
   constructor(
     private _usuarioService: UsuarioService,
     private toastr: ToastrService
@@ -28,14 +26,13 @@ export class DInicioComponent implements OnInit {
     // this.obtenerPacientes();
     // this.obtenerExpedientes();
     this.obtenerUsuarios();
-    this.obtenerRoles();
     //this.fnchola();
   }
   fnchola(){
-    console.log("Hola");
-    this.listUsuarios.forEach(lista =>{
-      console.log(lista.usuario_rol.desRol)
-    })
+    //console.log("Hola");
+    let dom = this.listUsuarios[0].usuario_persona[0].persona_domicilio[0].calle;
+    console.log(dom);
+    
   }
 
   obtenerUsuarios() {
@@ -47,22 +44,6 @@ export class DInicioComponent implements OnInit {
           },error => {
             console.log(error);
           });
-  }
-  obtenerRoles(){
-    this._usuarioService.getUsuarios().subscribe(data => {
-      for (let index = 0; index < data.length; index++) {
-        //console.log(index);
-        //console.log(data[index].usuario_rol[0].desRol);
-        let rol = data[index].usuario_rol;
-        this.listUsuarios_Rol.push(rol);
-      }
-      //console.log(this.listUsuarios_Rol);
-      //this.toastr.success('Roles cargados con éxito','Roles cargados');
-            
-      //console.log(data.length)
-    },error => {
-      console.log(error);
-    });
   }
   
   
