a2596a80df417ed4794469ae6a311a63bc586c10
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-11-01 00:16:10 -0700

    sergio

--- src/app/components/p-inicio/p-inicio.component.ts
+++ src/app/components/p-inicio/p-inicio.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 @Component({
   selector: 'app-p-inicio',
   templateUrl: './p-inicio.component.html',
-  styleUrls: ['./p-inicio.component.css']
+  styleUrls: ['./p-inicio.component.css'],
 })
 export class PInicioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  id: string;
+  usuarioForm: FormGroup;
+  usuario: Usuario | null;
+  nombre: string;
+  constructor(
+    private _usuarioService: UsuarioService,
+    private fb: FormBuilder,
+    private aRouter: ActivatedRoute
+  ) {
+    this.usuarioForm = this.fb.group({
+      nombre: ['', Validators.required],
+    });
+    this.id = this.aRouter.snapshot.paramMap.get('id') + '';
+    this.usuario = null;
+    this.nombre = '';
   }
 
+  ngOnInit(): void {
+    console.log('Desde inicio: ' + this.id);
+    this.obtenerUsuario();
+  }
+
+  obtenerUsuario() {
+    if (this.id !== '') {
+      this._usuarioService.obtenerUsuario(this.id).subscribe((data) => {
+        console.log(data);
+        console.log(data.usuario_persona.nombre);
+        this.usuario = data;
+        this.nombre = this.usuario?.usuario_persona.nombre + '';
+      });
+    }
+  }
 }
--- src/app/components/t-inicio/t-inicio.component.ts
+++ src/app/components/t-inicio/t-inicio.component.ts
@@ -7,7 +7,9 @@ import { findIndex, Observable } from 'rxjs';
 // import { PacienteService } from 'src/app/services/paciente.service';
 //import { AuthService } from '@auth0/auth0-angular';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { PacienteService } from 'src/app/services/paciente.service';
 import {Usuario} from 'src/app/models/usuario'
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-d-inicio',
@@ -16,35 +18,67 @@ import {Usuario} from 'src/app/models/usuario'
 })
 export class TInicioComponent implements OnInit {
   listUsuarios: Usuario[] = [];
+  id: string;
+  usuario: Usuario | null;
+  nombre: string;
   constructor(
     private _usuarioService: UsuarioService,
+    private _pacienteService: PacienteService,
+    private aRouter: ActivatedRoute,
     private toastr: ToastrService
   ) 
-  {}
+  {
+    this.id = this.aRouter.snapshot.paramMap.get('id') + '';
+    this.usuario = null;
+    this.nombre = '';
+  }
 
   ngOnInit(): void {
     // this.obtenerPacientes();
     // this.obtenerExpedientes();
     this.obtenerUsuarios();
+    this.obtenerUsuario();
     //this.fnchola();
   }
   fnchola(){
     //console.log("Hola");
     // let dom = this.listUsuarios[0].usuario_persona[0].persona_domicilio[0].calle;
     // console.log(dom);
-    
+     
   }
-
+ 
   obtenerUsuarios() {
-    this._usuarioService.getUsuarios().subscribe(data => {
+    this._pacienteService.getPacientes().subscribe(data => {
             console.log(data);
             //console.log(data.length)
             //this.toastr.success('Usuarios cargados con éxito','Usuarios cargados');
             this.listUsuarios =data;
+            console.log(data[0].usuario)
           },error => {
             console.log(error);
           });
   }
+  // obtenerPacientesActivos(){
+  //   this._pacienteService.getPacientesActivos().subscribe(data => {
+  //     console.log(data);
+  //     //console.log(data.length)
+  //     //this.toastr.success('Usuarios cargados con éxito','Usuarios cargados');
+  //     this.listUsuarios =data;
+  //   },error => {
+  //     console.log(error);
+  //   });
+  // }
+  obtenerUsuario() {
+    if (this.id !== '') {
+      this._usuarioService.obtenerUsuario(this.id).subscribe((data) => {
+        //console.log(data);
+        //console.log(data.usuario_persona.nombre);
+        this.usuario = data;
+        this.nombre = this.usuario?.usuario_persona.nombre + '';
+      }); 
+    }
+  }
+  
   
   
 }
