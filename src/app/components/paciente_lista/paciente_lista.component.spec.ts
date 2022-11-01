3afef31311154d2057be13968bf3721579fd5a16
SergioSerrano30 <84033208+SergioSerrano30@users.noreply.github.com>
2022-11-01 13:55:15 -0700

    sergio

---
+++ src/app/components/paciente_lista/paciente_lista.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TInicioComponent } from './paciente_lista.component';
+
+describe('TInicioComponent', () => {
+  let component: TInicioComponent;
+  let fixture: ComponentFixture<TInicioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TInicioComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TInicioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/terapeuta_inicio/terapeuta_inicio.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
+@Component({
+  selector: 'app-terapeuta',
+  templateUrl: './terapeuta_inicio.component.html',
+  styleUrls: ['./terapeuta_inicio.component.css'],
+})
+export class TerapeutaComponent implements OnInit {
+  id: string;
+  usuarioForm: FormGroup;
+  usuario: Usuario | null;
+  nombre: string;
+  constructor(
+    private _usuarioService: UsuarioService,
+    private fb: FormBuilder,
+    private aRouter: ActivatedRoute,
+    private router:Router
+  ) { 
+    this.usuarioForm = this.fb.group({
+      nombre: ['', Validators.required],
+    });
+    this.id = this.aRouter.snapshot.paramMap.get('id') + '';
+    this.usuario = null;
+    this.nombre = '';
+  }
+
+  ngOnInit(): void {
+    this.obtenerUsuario();
+  }
+  obtenerUsuario() {
+    if (this.id !== '') {
+      this._usuarioService.obtenerUsuario(this.id).subscribe((data) => {
+        //console.log(data);
+        //console.log(data.usuario_persona.nombre);
+        this.usuario = data;
+        this.nombre = this.usuario?.usuario_persona.nombre + '';
+      }); 
+    }
+  }
+  irPacientes(){
+    this.router.navigate(['/terapeuta-inicio-pacientes/'+this.id]);
+  }
+  irInicio(){
+    let rol = this.usuario?.usuario_rol.desRol;
+    switch (rol) {
+      case "Paciente":
+        this.router.navigate(['/paciente-inicio/' + this.id])
+        break;
+        case "Administrador":
+        this.router.navigate(['/admin-inicio/' + this.id])
+        break;
+        case "Terapeuta":
+        this.router.navigate(['/terapeuta-inicio/' + this.id])
+        break;
+    
+      default:
+        break;
+    }
+  }
+  irLoginD(){
+    this.router.navigate(['/loginD'])
+  }
+}
