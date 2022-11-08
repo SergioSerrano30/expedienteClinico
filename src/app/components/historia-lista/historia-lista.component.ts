82cad3c316f28c1d2056fd6fe4ccdf28a6969537
ChristopherRamosM <chjeramosmo@ittepic.edu.mx>
2022-11-08 03:03:53 -0700

    cards

---
+++ src/app/components/historia-lista/historia-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historia-lista',
+  templateUrl: './historia-lista.component.html',
+  styleUrls: ['./historia-lista.component.css']
+})
+export class HistoriaListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
